b39146298b19421f371427eb86431d69d2179d9d
Yuriy Bahur <yuriybahur1@gmail.com>
2019-05-13 05:50:33 +0300

    add

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,41 +1,92 @@
 $(function () {
 
-	$('.form__ingidient-button').on('click', function () {
-		const template = $(".form__ingidient-wrapper").filter(':hidden')
-		const clonedTemplate = template.clone(true)
-		const closedIcon = clonedTemplate.find('.form__ingidient-close')
-		const container = $(".form__ingridients")
-		container.append(clonedTemplate)
-		clonedTemplate.show()
-		closedIcon.addClass('is-shown')
-	});
+	function addIngredientInput() {
+		let numberClicked = 1
 
-	$(document).on('click', '.form__ingidient-close', function () {
+		$('.form__ingidient-button').on('click', function () {
+			const template = $(".form__ingidient-wrapper").filter(':hidden')
+			const clonedTemplate = template.clone(true)
+			const input = clonedTemplate.find('.form__field')
+			const closedIcon = clonedTemplate.find('.form__ingidient-close')
+			const container = $(".form__ingridients")
+			container.append(clonedTemplate)
+			clonedTemplate.show()
+			closedIcon.addClass('is-shown')
+			input.attr('name', `ingredient${numberClicked}`)
+			numberClicked++
+		});
+	}
+
+	addIngredientInput()
+
+
+	function removeIngrediment() {
 		const that = $(this)
 		const parent = that.closest('.form__ingidient-wrapper')
 		parent.remove()
-	});
+	}
 
-	$.validator.setDefaults({
-		submitHandler: function () {
-			alert("submitted!");
+	$('.form__ingidient-close').on('click', removeIngrediment);
+
+
+	function handleSubmitForm() {
+		event.preventDefault()
+
+		$('.form__field--ingredient').each(function () {
+			$(this).rules("add", {
+				required: true,
+				minlength: 5,
+				messages: {
+					required: "Please enter ingredient",
+					minlength: "Ingredient must consist at least 5 characters"
+				}
+			})
+		});
+
+		//if form submitted
+		if ($(this).validate().form()) {
+			alert('submitted')
 		}
-	});
+	}
+
+	$("#form").on('submit', handleSubmitForm)
 
 	$("#form").validate({
 		rules: {
-			title: "required",
-			description: "required",
-			imageURL: "required",
-			instructions: "required",
-			ingredient: "required",
+			title: {
+				required: true,
+				minlength: 5
+			},
+			description: {
+				required: true,
+				minlength: 26
+			},
+			imageURL: {
+				required: true,
+				url: true
+			},
+			instructions: {
+				required: true,
+				minlength: 26
+			},
 		},
 		messages: {
-			title: "Please enter title",
-			description: "Please enter description",
-			imageURL: "Please provide image URL",
-			instructions: "Please provide instructions",
-			ingredient: "Please provide ingredient",
+			title: {
+				required: "Please enter title",
+				minlength: "Title must consist at least 5 characters"
+			},
+			description: {
+				required: "Please provide description",
+				minlength: "Description must consist at least 26 characters"
+			},
+			imageURL: {
+				required: "Please provide image URL",
+				url: "Please provide valid URL"
+			},
+			instructions: {
+				required: "Please provide instructions",
+				minlength: "instructions must consist at least 26 characters"
+			},
 		}
 	});
 
